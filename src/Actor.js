48feb33d9f4b8d82924f75e21cf45055638596cf
aidanakmatova13 <akmatova.don@gmail.com>
2021-09-05 02:28:59 +0300

    fix box-style

--- src/Actor.js
+++ src/Actor.js
@@ -58,10 +58,12 @@ const Actor = () =>{
             <div className='grid-2' key={movies.id}>
                 {
                     movies.map(el =>
+                        <div className='box'>
                         <Link to={`/movie/${el.id}`} key={el.id}>
                             <img src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${el.poster_path}`} alt=""/>
                             <h3>{el.original_title}</h3>
                         </Link>
+                        </div>
                     )
                 }
             </div>
