d5123824df135547720c008b3ece82dabf218c3c
aidanakmatova13 <akmatova.don@gmail.com>
2021-09-08 14:41:03 +0300

    fix input

--- src/views/Header.js
+++ src/views/Header.js
@@ -1,5 +1,5 @@
 import logo from '../components/Image/movie-time-cinema-logo-8B5BE91828-seeklogo.com.png'
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import Loading from "../components/Loading";
 import {useState} from "react";
 import loop from './svg/free-icon-magnifying-glass-search-16492.svg';
@@ -7,9 +7,17 @@ import loop from './svg/free-icon-magnifying-glass-search-16492.svg';
 const Header = () =>{
     const [isLoading, setIsLoading] = useState(false)
     const [search, setSearch] = useState('')
+    const history = useHistory();
     const handleSearch = (e) =>{
         setSearch(e.target.value)
     }
+    const btnSearch = () => {
+        if (search.trim()) {
+            history.push(`/search/${search}`)
+            setSearch("")
+        }
+    }
+
     if (isLoading){
         return <Loading/>
     }
@@ -19,10 +27,10 @@ const Header = () =>{
                 <Link to={'/'}>
                     <img className='logo-img' src={logo} alt=""/>
                 </Link>
-                <div className='flex'><input placeholder='search...' type="text" onChange={handleSearch}/>
-                    <Link to={`/search/${search}`}>
-                        <img className='search-btn' src={loop} alt=""/>
-                    </Link>
+                <div className='flex'><input value={search} onKeyDown={e => {if(e.key === "Enter") btnSearch()}} placeholder='search...' type="text" onChange={handleSearch}/>
+                    <button className='search-btn' onClick={btnSearch}>
+                        <img  src={loop} alt=""/>
+                    </button>
                 </div>
             </div>
         </div>
