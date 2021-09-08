8c885e9c936958b15c574e285a2d4d6e5ff8c533
aidanakmatova13 <akmatova.don@gmail.com>
2021-09-08 20:24:52 +0300

    add media

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './media.css';
 import './style.css';
 import './index.scss';
-import './media.css';
 
 
 ReactDOM.render(<App />,document.getElementById('root'));
